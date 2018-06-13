8fe87fa1eac1c2e6a2695e1c370e217b2c34d33a
Marco <marco.egbring@epha.ch>
2018-06-13 18:50:39 +0200

    add autotop

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -9,11 +9,11 @@ then
   aws --region eu-central-1 s3 sync --delete assets/ s3://epha.io/datensatz/assets/
   aws --region eu-central-1 s3 sync --delete data/ s3://epha.io/datensatz/data/
 
-  aws --region eu-central-1 s3 sync --delete index.html s3://epha.io/datensatz/index.html
-  aws --region eu-central-1 s3 sync --delete _coverpage.md s3://epha.io/datensatz/_coverpage.md
-  aws --region eu-central-1 s3 sync --delete SUMMARY.md s3://epha.io/datensatz/SUMMARY.md
-  aws --region eu-central-1 s3 sync --delete README.md s3://epha.io/datensatz/README.md
-  aws --region eu-central-1 s3 sync --delete LICENSE.md s3://epha.io/datensatz/LICENSE.md
+  aws --region eu-central-1 s3 cp index.html s3://epha.io/datensatz/index.html
+  aws --region eu-central-1 s3 cp _coverpage.md s3://epha.io/datensatz/_coverpage.md
+  aws --region eu-central-1 s3 cp SUMMARY.md s3://epha.io/datensatz/SUMMARY.md
+  aws --region eu-central-1 s3 cp README.md s3://epha.io/datensatz/README.md
+  aws --region eu-central-1 s3 cp LICENSE.md s3://epha.io/datensatz/LICENSE.md
   aws --region eu-central-1 s3 sync --delete docs/ s3://epha.io/datensatz/docs/
 
   aws cloudfront create-invalidation --distribution-id 	E3O8ET4HHUEUOM --paths /datensatz/* 1> /dev/null
@@ -25,11 +25,11 @@ then
   aws --region eu-central-1 s3 sync --delete assets/ s3://epha.ch/datensatz/assets/
   aws --region eu-central-1 s3 sync --delete data/ s3://epha.ch/datensatz/data/
 
-  aws --region eu-central-1 s3 sync --delete index.html s3://epha.ch/datensatz/index.html
-  aws --region eu-central-1 s3 sync --delete _coverpage.md s3://epha.ch/datensatz/_coverpage.md
-  aws --region eu-central-1 s3 sync --delete SUMMARY.md s3://epha.ch/datensatz/SUMMARY.md
-  aws --region eu-central-1 s3 sync --delete README.md s3://epha.ch/datensatz/README.md
-  aws --region eu-central-1 s3 sync --delete LICENSE.md s3://epha.ch/datensatz/LICENSE.md
+  aws --region eu-central-1 s3 cp index.html s3://epha.ch/datensatz/index.html
+  aws --region eu-central-1 s3 cp _coverpage.md s3://epha.ch/datensatz/_coverpage.md
+  aws --region eu-central-1 s3 cp SUMMARY.md s3://epha.ch/datensatz/SUMMARY.md
+  aws --region eu-central-1 s3 cp README.md s3://epha.ch/datensatz/README.md
+  aws --region eu-central-1 s3 cp LICENSE.md s3://epha.ch/datensatz/LICENSE.md
   aws --region eu-central-1 s3 sync --delete docs/ s3://epha.ch/datensatz/docs/
 
   aws cloudfront create-invalidation --distribution-id 	ES8QILS3Z5OA1 --paths /datensatz/* 1> /dev/null
