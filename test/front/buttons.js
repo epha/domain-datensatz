77027df058b26434b633a0f3405fc5bc02003f84
Jonas74 <yomarco.egbring@epha.ch>
2018-07-26 02:56:01 +0200

    fkx test

--- test/front/buttons.js
+++ test/front/buttons.js
@@ -10,23 +10,23 @@ const os = require("os")
 // //localhost:13457/assets
 // //localhost:13457/datensatz
 
-const app = express()
+//const app = express()
 
-app.use("/", express.static('./node_modules/domain-akte'))
-app.use('/assets', express.static( './node_modules/domain-assets/assets') )
-app.use('/datensatz', express.static( './datensatz')  )
-app.use('/datensatz/docs/starten', express.static( './datensatz/index.html')  )
+//app.use("/", express.static('./node_modules/domain-akte'))
+//app.use('/assets', express.static( './node_modules/domain-assets/assets') )
+//app.use('/datensatz', express.static( './datensatz')  )
+//app.use('/datensatz/docs/starten', express.static( './datensatz/index.html')  )
 
-const hosting = app.listen(13457)
+//const hosting = app.listen(13457)
 
 tape('front buttons', async (t) => {
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  //await page.goto('https://epha.ch/', { waitUntil: 'networkidle2'});
-  await page.goto(`http:///${os.hostname()}:13457/datensatz/docs/starten`, { waitUntil: 'networkidle2' });
-  await page.screenshot({path: './test/front/screenshot.png'});
+  await page.goto('https://epha.ch/', { waitUntil: 'networkidle2'});
+  //page.goto(`http:///${os.hostname()}:13457/datensatz/docs/starten`, { waitUntil: 'networkidle2' });
+  //page.screenshot({path: './test/front/screenshot.png'});
 
   // Get the "viewport" of the page, as reported by the page.
   // const dimensions = await page.evaluate(() => {
