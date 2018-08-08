e8aede467b700edff4553a8ff90ada11c4ce5cb4
Marco <marco.egbring@epha.ch>
2018-08-08 13:13:36 +0200

    fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,8 +14,8 @@ fi
 
 if [[ $1 == "release" ]]
 then
-  printf "  Upload epha.ch/assets \n"
-  
+  printf "  Upload epha.ch/datensatz/ \n"
+
   aws --region eu-central-1 s3 sync --delete datensatz/ s3://epha.ch/datensatz/ --exclude .DS_Store
 
   aws cloudfront create-invalidation --distribution-id 	ES8QILS3Z5OA1 --paths /datensatz/* 1> /dev/null
