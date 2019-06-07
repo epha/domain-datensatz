0b1786414bbd25e0a13eb07b03bb91fa12b76bca
Marco <marco.egbring@epha.ch>
2019-06-07 18:44:07 +0200

    make test better report the problematic codes

--- test/unit/atcZiffern.js
+++ test/unit/atcZiffern.js
@@ -1,61 +1,61 @@
 const tape = require( "tape" )
 
-const { wirkstoffe } = require( '../..' )
+const { wirkstoffe, artikel } = require( '../..' )
 
 tape.only( "Unit atc ziffern", async ( t ) => {
 
-  const a1d = Object.keys( wirkstoffe ).filter( w => {
-    return w.length == 1
-  } )
-  const a3d = Object.keys( wirkstoffe ).filter( w => {
-    return w.length == 3
-  } )
-  const a4d = Object.keys( wirkstoffe ).filter( w => {
-    return w.length == 4
-  } )
-  const a5d = Object.keys( wirkstoffe ).filter( w => {
-    return w.length == 5
-  } )
-  const a7d = Object.keys( wirkstoffe ).filter( w => {
-    return w.length == 7
-  } )
+  // -----------------------------------------
+  // Build object with used keys in packungen
+  // and build up the hierarchy
+  // -----------------------------------------
+  const relevant = Object.values( artikel ).reduce( ( acc, cur ) => {
+    acc[ cur.atcCode.substr( 0, 7 ) ] = true
+    acc[ cur.atcCode.substr( 0, 5 ) ] = true
+    acc[ cur.atcCode.substr( 0, 4 ) ] = true
+    acc[ cur.atcCode.substr( 0, 3 ) ] = true
+    acc[ cur.atcCode.substr( 0, 1 ) ] = true
+    return acc
+  }, {} )
 
-  const missing = []
-  const toomuch = []
 
-  const relevant = {}
+  // -----------------------------------------
+  // Wirkstoffe has defined codes noone needs
+  // (except) maybe Swissmedic
+  // -----------------------------------------
+  const toomuch = Object.keys( wirkstoffe ).filter( code => {
+    return !relevant[ code ]
+  } ).sort( ( a, b ) => a.localeCompare( b ) )
 
-  a7d.forEach( code => {
-
-    relevant[ code.substr( 0, 5 ) ] = true
-    if ( !a5d.includes( code.substr( 0, 5 ) ) ) {
-      missing.push( code.substr( 0, 5 ) )
-    }
-
-    relevant[ code.substr( 0, 4 ) ] = true
-    if ( !a4d.includes( code.substr( 0, 4 ) ) ) {
-      missing.push( code.substr( 0, 4 ) )
-    }
-
-    relevant[ code.substr( 0, 3 ) ] = true
-    if ( !a3d.includes( code.substr( 0, 3 ) ) ) {
-      missing.push( code.substr( 0, 3 ) )
-    }
-
-    relevant[ code.substr( 0, 1 ) ] = true
-    if ( !a1d.includes( code.substr( 0, 1 ) ) ) {
-      missing.push( code.substr( 0, 1 ) )
-    }
-
+  // --------------------------------------------------
+  // Wirkstoffe has not defined necessary code needed
+  // in artikel (packungen)
+  // --------------------------------------------------
+  const missing = Object.keys( relevant ).filter( code => {
+    return !wirkstoffe[ code ]
   } )
 
-  a5d.filter( code => !relevant[ code ] ).forEach( code => toomuch.push( code ) )
-  a4d.filter( code => !relevant[ code ] ).forEach( code => toomuch.push( code ) )
-  a3d.filter( code => !relevant[ code ] ).forEach( code => toomuch.push( code ) )
-  a1d.filter( code => !relevant[ code ] ).forEach( code => toomuch.push( code ) )
+  if ( toomuch.length > 0 || missing.length > 0 ) {
+    t.fail( "Codes imperfect" )
+  }
 
-  t.equals( missing.length, 0, "All substances have code up the hierarchy" )
-  t.equals( toomuch.length, 0, "All codes have corresponding substances" )
+  if ( toomuch.length > 0 ) {
+    console.log( "---" )
+    console.log( "Wirkstoffe provides codes not used in artikel (packungen)" )
+    console.log( "(except maybe Swissmedic)" )
+    console.log( "---" )
+    toomuch.forEach( ( code, idx ) => console.log( `${String(idx).padStart(3)} ${code}` ) )
+  } else {
+    t.ok( "All wirkstoffe codes are used in artkel" )
+  }
+
+  if ( missing.length > 0 ) {
+    console.log( "---" )
+    console.log( "These codes in artikel (packungen) are not provided in wirkstoffe" )
+    console.log( "---" )
+    missing.forEach( ( code, idx ) => console.log( `${String(idx).padStart(3)} ${code}` ) )
+  } else {
+    t.ok( "All artikel codes are provided in wirkstoffe" )
+  }
 
   t.end()
 
